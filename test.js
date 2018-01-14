c7b3b8803c151ce4db287f3c93da70a1ff8f0702
Novan Andriyono <evelynradtya@gmail.com>
2018-01-14 22:56:14 +0700

    closing test

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
-var pyfilesCopy = require('./index.js');
-var options = {
-	'fromDir': './node_modules',
-	'toDir': './tests',
-	'formatFile': 'js',
-	'srcFiles': false,
-	'except': ['.git','tests'],
-}
-console.log(pyfilesCopy(options));
+// var path = require('path');
+// var pyfilesCopy = require('./index.js');
+// var baseFolder = __dirname + path.sep ;
+// var fromDir =  '../../my-git-repositories/mdc-sass';
+// var except1 = fromDir + path.sep + '.git';
+// var except2 = fromDir + path.sep + 'node_modules';
+// var options = {
+// 	'fromDir': fromDir,
+// 	'toDir': './tests',
+// 	'formatFile': 'scss',
+// 	'srcFiles': false,
+// 	'except': [except1,except2],
+// 	'folderMode': '0755',
+// 	'fileMode': '0644',
+// }
+// console.log(pyfilesCopy(options));
